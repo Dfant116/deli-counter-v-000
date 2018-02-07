755aecaeb97082a2a604d428a0f1825f5f975b9a
Dalton Fant <daltonfant16@gmail.com>
2018-02-07 21:21:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ def line(num_in_line)
   line_array = []
   if num_in_line.length == 0
     puts "The line is currently empty."
-  else 
+  else
     num_in_line.each.with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
+
